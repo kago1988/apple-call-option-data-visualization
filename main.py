552417d21992b909deb37faa1f34fd2ca3c717aa
Andreas Kagoshima <a.kago1988@gmail.com>
2022-07-07 11:26:30 +0200

    First commit

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from calculate_greeks import Greeks
 
 
 
-AAPL_stock = pd.read_csv('AAPL_stock.csv')
-AAPL_options = pd.read_csv('AAPL_options.csv')
+AAPL_stock = pd.read_csv('./data/AAPL_stock.csv')
+AAPL_options = pd.read_csv('./data/AAPL_options.csv')
 AAPL_stock_vol = AAPL_stock['Volume']
 AAPL_stock_spread = AAPL_stock['High'] - AAPL_stock['Low']
 AAPL_stock_date = AAPL_stock['Date'] 
@@ -25,7 +25,7 @@ underlying_price_series = C.get_underlying_prices()
 # based on scattered "expiration date days"
 scattered_expiration_days_series = C.get_scattered_overlap_dates()
 scattered_strike_price_plus_ask_price_series = C.get_scattered_strike_price_plus_ask_price() 
-# Calculating the "Greeks". We need an array, that contains for every row of AAPL_call_options the interest rate. See './interest_rate.jpg' 
+# Calculating the "Greeks". We need an array, that contains for every row of AAPL_call_options the interest rate. See './data/interest_rate.jpg' 
 # We also need an array, that contains for every row of AAPL_call_options the number of days until expiration. 
 call_option_interest_rate_series = C.get_option_interest_rate_series()
 time_until_expiration_series = C.get_time_until_expiration_series()
@@ -48,6 +48,6 @@ plot.show_correlation_2('Time', 'Trading Volume', 'CALL: (Strike + Ask) at Bid D
                 AAPL_call_options['DataDate'], (AAPL_call_options['Strike']+AAPL_call_options['Ask']))
 
 # CALCULATE GREEKS 
-# Implied volatility and interest rate: see "./implied_volatility.jpg" and "./interest_rate.jpg". 
+# Implied volatility and interest rate: see "./data/implied_volatility.jpg" and "./data/interest_rate.jpg". 
 G = Greeks('c', AAPL_call_options, call_option_interest_rate_series, time_until_expiration_series)
 G.return_greeks() 
